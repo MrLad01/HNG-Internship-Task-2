44ca85ea7ff45434dac2d96b7d5cf265fbfe9302
MrLad01 <ladayodeji@gmail.com>
2023-09-13 11:41:13 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "../src/Pages/subpages/Home.jsx",
+    "../src/Pages/subpages/Footer.jsx",
+    "../src/Pages/subpages/Feature.jsx",
+    "../src/Pages/subpages/Main.jsx",
+    "../src/Pages/subpages/SideBar.jsx",
+    "../src/Pages/Homepage.jsx",
+    "../src/Pages/MoviePage.jsx",
+  ],
   theme: {
     extend: {},
   },
