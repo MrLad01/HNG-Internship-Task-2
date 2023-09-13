6fdc170a5245d33f73e8ce87b5e0ff033bc0fb4a
MrLad01 <ladayodeji@gmail.com>
2023-09-13 15:33:27 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,12 +15,13 @@ const App = () => {
       const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=03f44ed3cf05195953f4b1cc6517a406&page=1&primary_release_date.gte=2023&sort_by=popularity.desc&year=2023');
       if(response.ok){
         const data = await response.json();
-        const film = data.slice(0, 9);
-        setMovies(film)
+        const film = data.results;
+        console.log(film);
+        setMovies(film);
       }
     }
     fetchMovies();
-  })
+  }, [])
 
   const router = createBrowserRouter(
     createRoutesFromElements(
--- frontend/src/Pages/subpages/Home.jsx
+++ frontend/src/Pages/subpages/Home.jsx
@@ -1,39 +1,48 @@
 import React from 'react'
 
-const Home = ({movies}) => (
-    <React.Fragment>
-        <nav>
-            <div className='logo'>
-                <img src="" alt="logo" />
-                <p></p>
-            </div>
-            <div id='search-bar'>
-                <input type="text" placeholder='What do you want to watch?' />
-                {/* search-icon */}
-            </div>
-            <div>
-                <a href="">Sign In</a>
-                {/* hamburger */}
-            </div>
-        </nav>
-        <section>
-            {movies}
-            <p id="title"></p>
-            <div className="movie-ratings">
-                <div className="imdb-rating">
-                    <img src="" alt="imdb logo" />
+const Home = ({movies}) => {
+    
+    const word = movies.map((e) => e.id)
+
+    return (
+        <React.Fragment>
+            <p>{word}</p>
+            <nav>
+                <div className='logo'>
+                    <img src="" alt="logo" />
+                    <p></p>
                 </div>
-                <div className="rotten-tomatoes-rating">
-                    <img src="" alt="rotten tomato logo" />
+                <div id='search-bar'>
+                    <input type="text" placeholder='What do you want to watch?' />
+                    {/* search-icon */}
                 </div>
-            </div>
-            <p className="movie-description"></p>
-            <button>
-                {/* play icon */}
-                WATCH TRAILER
-            </button>
-        </section>
-    </React.Fragment>
-)
+                <div>
+                    <a href="">Sign In</a>
+                    {/* hamburger */}
+                </div>
+            </nav>
+            <section>
+                <p>
+                    e
+
+                </p>
+                <p id="title"></p>
+                <div className="movie-ratings">
+                    <div className="imdb-rating">
+                        <img src="" alt="imdb logo" />
+                    </div>
+                    <div className="rotten-tomatoes-rating">
+                        <img src="" alt="rotten tomato logo" />
+                    </div>
+                </div>
+                <p className="movie-description"></p>
+                <button>
+                    {/* play icon */}
+                    WATCH TRAILER
+                </button>
+            </section>
+        </React.Fragment>
+    )
+}
 
 export default Home
