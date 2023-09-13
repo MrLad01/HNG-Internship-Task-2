58c75db8ecf0257495ebf4d455fb41bf351baaa8
MrLad01 <ladayodeji@gmail.com>
2023-09-13 04:38:00 +0100

    update

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Home from './subpages/Home'
+import Feature from './subpages/Feature'
 
 const Homepage = () => {
   return (
     <>
-      
+      <Home />
+      <Feature />
     </>
   )
 }
---
+++ frontend/src/Pages/subpages/Feature.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../../Components/Card'
+
+const Feature = () => {
+  return (
+    <React.Fragment>
+      <header>Featured Movie</header>
+      <div>
+        <Card />
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Feature
