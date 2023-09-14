eb8e61d3c9244e74ae75a7118176bef05682a859
MrLad01 <ladayodeji@gmail.com>
2023-09-14 20:46:30 +0100

    update

--- frontend/src/Pages/subpages/Home.jsx
+++ frontend/src/Pages/subpages/Home.jsx
@@ -3,6 +3,7 @@ import movieboxLogo from '../../assets/tv.png'
 import menu from '../../assets/Menu.png'
 import { Link } from 'react-router-dom'
 import play from "../../assets/PlayIcon.png"
+// import fetchRandomImageWithOverview from "../../helpers"
 
 
 const Home = ({movies}) => {
@@ -10,33 +11,47 @@ const Home = ({movies}) => {
     const [randomImageInfo, setRandomImageInfo] = useState(null);
    
   useEffect(() => {
-    if (!randomImageInfo) {
-      async function fetchRandomImageWithOverview() {
-        try{
-          const movieBackdrops = movies.map(movie => ({
-            backdropUrl: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
-            overview: movie.overview,
-            title: movie.original_title
-          }));
+    if (!randomImageInfo && movies && movies.length > 0) {
+        const fetchRandomImageWithOverview = async () => {
+            try{
+              const movieBackdrops = await movies.map(movie => ({
+                backdropUrl: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
+                overview: movie.overview,
+                title: movie.original_title
+              }));
+        
+              // Choose a random movie backdrop
+              
+              if (movieBackdrops.length === 0) {
+                  console.error('No movie backdrops available.');
+                  return;
+                }
+                
+            const randomBackdropInfo = movieBackdrops[Math.floor(Math.random() * movieBackdrops.length)];
 
-          // Choose a random movie backdrop
-          const randomBackdropInfo = movieBackdrops[Math.floor(Math.random() * movieBackdrops.length)];
 
-          // Update state with the random image and its metadata
-          setRandomImageInfo(randomBackdropInfo);
-        } catch (error) {
-          console.error('Error fetching TMDb data:', error);
-        }
-      }
+              console.log('Random backdrop info:', randomBackdropInfo);
 
-      fetchRandomImageWithOverview();
+              if (movieBackdrops.length === 0) {
+                console.error('No movie backdrops available.');
+                return;
+              }
+        
+              // Update state with the random image and its metadata
+              setRandomImageInfo(randomBackdropInfo);
+            } catch (error) {
+              console.error('Error fetching TMDb data:', error);
+            }
+          }
+        
+          fetchRandomImageWithOverview();
     }
-  }, [randomImageInfo]); 
+  }, [movies, randomImageInfo]); 
 
     return (
         <React.Fragment>
-            <div className='h-screen bg-cover bg-center text-white' style={{backgroundImage:`url(${randomImageInfo?.backdropUrl})`}} >
-                <div className="container w-full h-full xs:mx-1 xs:pt-1 sm:mx-3 sm:pt-1 md:mx-4 md:pt-2 lg:mx-7 lg:pt-3 xl:mx-9 xl:pt-4 2xl:mx-12 2xl:pt-6 items-start" >
+            <div className='h-screen w-full bg-cover bg-center text-white' style={{backgroundImage:`url(${randomImageInfo?.backdropUrl})`}} >
+                 <div className="container w-full h-full xs:mx-1 xs:pt-1 sm:mx-3 sm:pt-1 md:mx-4 md:pt-2 lg:mx-7 lg:pt-3 xl:mx-9 xl:pt-4 2xl:mx-12 2xl:pt-6 items-start" >
                     <img src="" alt="" />
                     <nav className='flex w-full justify-between'>
                         <Link className='logo flex items-center' to='/'>
@@ -56,7 +71,7 @@ const Home = ({movies}) => {
                         <p>
 
                         </p>
-                        <p id="title">{randomImageInfo.title}</p>
+                        <p id="title">{randomImageInfo?.title}</p>
                         <div className="movie-ratings">
                             <div className="imdb-rating">
                                 <img src="" alt="imdb logo" />
@@ -65,7 +80,9 @@ const Home = ({movies}) => {
                                 <img src="" alt="rotten tomato logo" />
                             </div>
                         </div>
-                        <p className="movie-description"></p>
+                        <p className="movie-description">
+                            {randomImageInfo?.overview}
+                        </p>
                         <button className='bg-rose-700 flex text-white items-center md:px-4 lg:px-4 xl:px-6 2xl:px-7  md:py-3 lg:py-3 xl:py-4 2xl:py-5 md:rounded-lg'>
                             <img src={play} alt="" className='mr-2' />
                             WATCH TRAILER
