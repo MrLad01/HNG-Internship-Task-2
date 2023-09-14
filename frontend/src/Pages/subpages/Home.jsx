dc39db1ed7b793b3c91ace3d4311a061fbfd955f
MrLad01 <ladayodeji@gmail.com>
2023-09-14 19:55:40 +0100

    update

--- frontend/src/Pages/subpages/Home.jsx
+++ frontend/src/Pages/subpages/Home.jsx
@@ -1,49 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import movieboxLogo from '../../assets/tv.png'
 import menu from '../../assets/Menu.png'
 import { Link } from 'react-router-dom'
+import play from "../../assets/PlayIcon.png"
+
 
 const Home = ({movies}) => {
-    
-    const word = movies.map((e) => e.id)
+
+    const [randomImageInfo, setRandomImageInfo] = useState(null);
+   
+  useEffect(() => {
+    if (!randomImageInfo) {
+      async function fetchRandomImageWithOverview() {
+        try{
+          const movieBackdrops = movies.map(movie => ({
+            backdropUrl: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
+            overview: movie.overview,
+            title: movie.original_title
+          }));
+
+          // Choose a random movie backdrop
+          const randomBackdropInfo = movieBackdrops[Math.floor(Math.random() * movieBackdrops.length)];
+
+          // Update state with the random image and its metadata
+          setRandomImageInfo(randomBackdropInfo);
+        } catch (error) {
+          console.error('Error fetching TMDb data:', error);
+        }
+      }
+
+      fetchRandomImageWithOverview();
+    }
+  }, [randomImageInfo]); 
 
     return (
         <React.Fragment>
-            <div className="container w-full h-full xs:mx-1 xs:pt-1 sm:mx-3 sm:pt-1 md:mx-4 md:pt-2 lg:mx-7 lg:pt-3 xl:mx-9 xl:pt-4 2xl:mx-12 2xl:pt-6 items-start">
-                <nav className='flex w-full justify-between'>
-                    <Link className='logo flex items-center' to='/'>
-                        <img src={movieboxLogo} alt="logo" className='mr-4  xs:ml-1 sm:ml-2' />
-                        <p>MovieBox</p>
-                    </Link>
-                    <div id='search-bar'>
+            <div className='h-screen bg-cover bg-center text-white' style={{backgroundImage:`url(${randomImageInfo?.backdropUrl})`}} >
+                <div className="container w-full h-full xs:mx-1 xs:pt-1 sm:mx-3 sm:pt-1 md:mx-4 md:pt-2 lg:mx-7 lg:pt-3 xl:mx-9 xl:pt-4 2xl:mx-12 2xl:pt-6 items-start" >
+                    <img src="" alt="" />
+                    <nav className='flex w-full justify-between'>
+                        <Link className='logo flex items-center' to='/'>
+                            <img src={movieboxLogo} alt="logo" className='mr-4  xs:ml-1 sm:ml-2' />
+                            <p>MovieBox</p>
+                        </Link>
+                        <div id='search-bar'>
                         <input type="text" placeholder='What do you want to watch?' />
-                        {/* search-icon */}
-                    </div>
-                    <div className='flex items-center'>
-                        <a href="">Sign In</a>
-                        <img src={menu} alt="menu" className='ml-5 xs:ml-1 sm:ml-2' />
-                    </div>
-                </nav>
-                <section>
-                    <p>
-                        e
+                            {/* search-icon */}
+                        </div>
+                        <div className='flex items-center'>
+                            <a href="">Sign In</a>
+                            <img src={menu} alt="menu" className='ml-5 xs:ml-1 sm:ml-2' />
+                        </div>
+                    </nav>
+                    <section>
+                        <p>
 
-                    </p>
-                    <p id="title"></p>
-                    <div className="movie-ratings">
-                        <div className="imdb-rating">
-                            <img src="" alt="imdb logo" />
+                        </p>
+                        <p id="title">{randomImageInfo.title}</p>
+                        <div className="movie-ratings">
+                            <div className="imdb-rating">
+                                <img src="" alt="imdb logo" />
+                            </div>
+                            <div className="rotten-tomatoes-rating">
+                                <img src="" alt="rotten tomato logo" />
+                            </div>
                         </div>
-                        <div className="rotten-tomatoes-rating">
-                            <img src="" alt="rotten tomato logo" />
-                        </div>
-                    </div>
-                    <p className="movie-description"></p>
-                    <button>
-                        {/* play icon */}
-                        WATCH TRAILER
-                    </button>
-                </section>
+                        <p className="movie-description"></p>
+                        <button className='bg-rose-700 flex text-white items-center md:px-4 lg:px-4 xl:px-6 2xl:px-7  md:py-3 lg:py-3 xl:py-4 2xl:py-5 md:rounded-lg'>
+                            <img src={play} alt="" className='mr-2' />
+                            WATCH TRAILER
+                        </button>
+                    </section>
+                </div>
             </div>
         </React.Fragment>
     )
