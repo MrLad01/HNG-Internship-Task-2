639ea7bfc8095c6996faa39e555404ec7cac3171
MrLad01 <ladayodeji@gmail.com>
2023-09-15 20:18:58 +0100

    update

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Card = ({movies}) => {
   const card = movies.map((e) => (
@@ -16,8 +16,8 @@ const Card = ({movies}) => {
       <div className='description xs:w-32 sm:w-36 md:w-64 lg:w-80 mt-3 gap-1'>
         <p className='movie-title xs:text-md sm:text-lg md:text-xl lg:text-2xl font-bold' data-testid='movie-title'>{e.title}</p>
         <span id='production-details' className='text-md text-gray-400'>release date: {e.release_date}</span>
-        <div className='movie-ratings'>
-          {/* star */}
+        <div className='movie-ratings gap-1 flex items-center'>
+          <FontAwesomeIcon icon="fa-solid fa-star" style={{color: "#fdd43f",}} />
           {e.vote_average}
         </div>
         <span id='movie-genre'></span>
