06e978bec1611396db29f9d57aea01e736a527c9
MrLad01 <ladayodeji@gmail.com>
2023-09-16 09:22:26 +0100

    update

--- frontend/src/Pages/subpages/Footer.jsx
+++ frontend/src/Pages/subpages/Footer.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {Link} from 'react-router-dom'
 
 const Footer = () => (
     <React.Fragment>
-        <div className="footer">
-            <div className="social-media">
+        <div className="footer grid justify-center my-10 gap-3">
+            <div className="social-media flex justify-self-center text-4xl gap-5">
+            <Link to='https://web.facebook.com' target='_blank'>
+             <FontAwesomeIcon icon="fa-brands fa-facebook" />
+            </Link>
+            <Link to='https://www.instagram.com' target='_blank'>
+             <FontAwesomeIcon icon="fa-brands fa-square-instagram" />
+            </Link>
+            <Link to="https://www.x.com" target='_blank'>
+             <FontAwesomeIcon icon="fa-brands fa-square-x-twitter" />
+            </Link>
+            <Link to="https://www.youtube.com" target='_blank'>
+             <FontAwesomeIcon icon="fa-brands fa-youtube" />
+            </Link>
                 {/* facebook */}
                 {/* instagram */}
                 {/* twitter */}
                 {/* youtube */}
             </div>
-            <div className="policies">
+            <div className="policies flex gap-4">
                 <a href="">Conditions of Use</a>
                 <a href="">Privacy & Policy</a>
                 <a href="">Press Room</a>
             </div>
+        <p id='copyright' className='justify-self-center'>&copy; 2021 MovieBox by Adriana Eka Prayudha</p>
         </div>
-        <p id='copyright'>&copy; 2021 MovieBox by Adriana Eka Prayudha</p>
     </React.Fragment>
 )
 
--- frontend/src/Pages/subpages/Home.jsx
+++ frontend/src/Pages/subpages/Home.jsx
@@ -3,6 +3,7 @@ import movieboxLogo from '../../assets/tv.png'
 import menu from '../../assets/Menu.png'
 import { Link } from 'react-router-dom'
 import play from "../../assets/PlayIcon.png"
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 
@@ -18,7 +19,8 @@ const Home = ({movies}) => {
                 backdropUrl: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
                 overview: movie.overview,
                 title: movie.original_title,
-                id: movie.id
+                id: movie.id,
+                ratings: movie.vote_average
               }));
         
               // Choose a random movie backdrop
@@ -63,35 +65,27 @@ const Home = ({movies}) => {
         <React.Fragment>
             <div className='h-screen w-full bg-cover bg-center text-white' style={{backgroundImage:`url(${randomImageInfo?.backdropUrl})`}} >
                  <div className="container w-full h-full xs:mx-1 xs:pt-1 sm:mx-3 sm:pt-1 md:mx-4 md:pt-2 lg:mx-7 lg:pt-3 xl:mx-9 xl:pt-4 2xl:mx-12 2xl:pt-6 items-start" >
-                    <img src="" alt="" />
-                    <nav className='flex w-full justify-between'>
+                    <nav className='flex md:w-full w-full justify-between items-center'>
                         <Link className='logo flex items-center' to='/'>
-                            <img src={movieboxLogo} alt="logo" className='mr-4  xs:ml-1 sm:ml-2' />
-                            <p>MovieBox</p>
+                            <img src={movieboxLogo} alt="logo" className='mr-4 xs:ml-1 sm:ml-2' />
+                            <p className='xs:text sm:text-base md:text-xl lg:text-2xl font-bold'>MovieBox</p>
                         </Link>
-                        <div id='search-bar'>
-                        <input type="text" placeholder='What do you want to watch?' />
-                            {/* search-icon */}
+                        <div id='search-bar flex items-center'>
+                        <input type="text" placeholder='What do you want to watch?' className='text-white bg-transparent border-2 rounded-lg p-1 border-white placeholder:text-white indent-1 outline-none xs:w-40 sm:w-48 md:w-72 lg:w-96' />
+                        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" style={{color: "#ffffff",}} className=' md:-ml-6 lg:-ml-6' />
                         </div>
                         <div className='flex items-center'>
-                            <a href="">Sign In</a>
+                            <a href="" className='text-base font-semibold'>Sign In</a>
                             <img src={menu} alt="menu" className='ml-5 xs:ml-1 sm:ml-2' />
                         </div>
                     </nav>
-                    <section>
-                        <p>
-
-                        </p>
-                        <p id="title">{randomImageInfo?.title}</p>
-                        <div className="movie-ratings">
-                            <div className="imdb-rating">
-                                <img src="" alt="imdb logo" />
-                            </div>
-                            <div className="rotten-tomatoes-rating">
-                                <img src="" alt="rotten tomato logo" />
-                            </div>
+                    <section className='xs:64 sm:w-72 md:w-80 lg:w-96 xs:mt-8 sm:mt-12 md:mt-16 lg:mt-20'>
+                        <p id="title" className='xs:text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold lg:my-auto'>{randomImageInfo?.title}</p>
+                        <div className="movie-ratings items-center flex">
+                        <FontAwesomeIcon icon="fa-solid fa-star" style={{color: "#ffffff",}} className='mr-1 lg:my-2' />
+                        <p id="ratings">{randomImageInfo?.ratings}</p> 
                         </div>
-                        <p className="movie-description">
+                        <p className="movie-description xs:text-xs sm:text-sm md:text-base lg:text-lg line-clamp-4 mb-3">
                             {randomImageInfo?.overview}
                         </p>
                         <a href={`/i/movies/${randomImageInfo?.id}`}>
